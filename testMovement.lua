bb931a7d30ff5e40475e16f3c421749cf51d1969
Office_PC <hskim@cs.duke.edu>
2015-09-23 18:11:00 -0400

    edit testMovement

--- testMovement.lua
+++ testMovement.lua
@@ -1,12 +1,31 @@
 scr = manager:machine().screens[":screen"]
 mem = manager:machine().devices[":maincpu"].spaces["program"]
-xadr = 0x060103a5
-yadr = 0x060103a9
+xlocadr = 0x060103a5
+ylocadr = 0x060103a9
+xveladr = 0x060103ad
+
+keyboardadr = 0x060103F5
 dir = 1
+keydir = "left"
+
+
+function cheat()
+-- set P1 invincible
+    mem:write_u8(0x60103FA, 1);
+end
+
+function update_p1()
+    p1_x = (mem:read_u32(0x60103a3) & 0xFFFF0) >> 8
+    p1_y = (mem:read_u32(0x60103a7) & 0xFFFF0) >> 8
+end
 
 function getState()
-  xloc = mem:read_u8(xadr)
-  yloc = mem:read_u8(yadr)
+  xloc = mem:read_u8(xlocadr)
+  yloc = mem:read_u8(ylocadr)
+  xvel = mem:read_u8(xveladr)
+  -- yvel = 
+  keypress = mem:read_u8(keyboardadr)
+  --print(keypress)
 end
 
 function drawBox_1P()
@@ -15,7 +34,7 @@ function drawBox_1P()
 end
 
 function move(direction, incr)
-  mem:write_u8(xadr, xloc+(direction*incr))
+  mem:write_u8(xlocadr, xloc+(direction*incr))
 end
 
 function moveLR()
@@ -27,10 +46,44 @@ function moveLR()
   move(dir,3)
 end
 
+function keyPress(key)
+  if key == "left" then
+    val = 16
+  elseif key == "right" then
+    val = 32
+  elseif key == "down" then
+    val = 64
+  elseif key == "up" then
+    val = 128
+  end
+  mem:write_u8(keyboardadr, val)
+end
+
+function moveKeyPress()
+  if xloc >= 213 then
+    keydir = "left"
+  elseif xloc <= 10 then
+    keydir = "right"
+  end
+  keyPress(keydir)  
+end
+
+
+function testKeyboard()
+  keypress(0x26)
+end
+
+
 function main()
+  cheat()
   getState()
-  moveLR()
+
+
+  --moveLR()
+  --moveKeyPress()
   drawBox_1P()
+
+  --testKeyboard()
 end
 
 emu.sethook(main, "frame")
